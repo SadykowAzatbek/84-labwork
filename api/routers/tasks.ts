fae3cea28f37766292a4a9276fc84647a5723f91
azatbek <sadykowazatbek@gmail.com>
2024-02-19 17:31:59 +0600

    Теперь при get запросе на /tasks будет доступны все задачи одного данного пользователя. При put запросе на /tasks/:id можно будет редактировать содержимое только данного пользователя.

--- api/routers/tasks.ts
+++ api/routers/tasks.ts
@@ -26,4 +26,50 @@ tasksRouter.post('/', auth, async (req: RequestWithUser, res, next) => {
   }
 });
 
+tasksRouter.get('/', auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const tasks = await Task.find({user: req.user?._id});
+
+    res.send(tasks);
+  } catch (err) {
+    next(err);
+  }
+});
+
+tasksRouter.put('/:id', auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const taskId = req.params.id;
+    const userId = req.user?._id;
+
+    const task = await Task.findOne({_id: taskId, user: userId});
+
+    if (!task) {
+      return res.status(403).send({error: `You don't have enough rights!!`});
+    }
+
+    if (req.body.title) {
+      task.title = req.body.title;
+    } else {
+      return res.status(422).send({error: 'the title must not be empty!'});
+    }
+
+    if (req.body.description) {
+      task.description = req.body.description;
+    }
+
+    if (req.body.status) {
+      task.status = req.body.status;
+    }
+
+    await task.save();
+    return res.send(task);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(err);
+    }
+
+    next(err);
+  }
+});
+
 export default tasksRouter;
